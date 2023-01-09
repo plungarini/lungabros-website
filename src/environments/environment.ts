5b128eaf58d211436c7fcf70c37dda2852485ba5
plungarini <pietro@lungarini.it>
2023-01-09 16:07:54 +0000

    deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
     projectId: 'lungabros',
     appId: '1:968081371580:web:739dcdaef6a5382b57fbb9',
     storageBucket: 'lungabros.appspot.com',
+    locationId: 'europe-west',
     apiKey: 'AIzaSyDG0zuPR_BjnjMw-ehZZ0vY_ntaV-dc6fM',
     authDomain: 'lungabros.firebaseapp.com',
     messagingSenderId: '968081371580',
